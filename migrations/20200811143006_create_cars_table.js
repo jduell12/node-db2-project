e092ff886248393fdf17e18237a2067cbe389529
Jessica <duellje@oregonstate.edu>
2020-08-11 14:43:44 -0400

    created cars database

--- migrations/20200811143006_create_cars_table.js
+++ migrations/20200811143006_create_cars_table.js
@@ -1,8 +1,21 @@
+exports.up = function (knex) {
+  return knex.schema.createTable('cars', tbl => {
+    tbl.increments('id')
+    tbl
+      .string('VIN')
+      .notNullable()
+      .unique()
+    tbl.string('make').notNullable()
+    tbl.string('model').notNullable()
+    tbl
+      .integer('mileage')
+      .notNullable()
+      .defaultTo(0)
+    tbl.string('transmission_type')
+    tbl.string('title_status')
+  })
+}
 
-exports.up = function(knex) {
-  
-};
-
-exports.down = function(knex) {
-  
-};
+exports.down = function (knex) {
+  return knex.schema.dropTableIfExists('cars')
+}
